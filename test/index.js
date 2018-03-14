a0956a03257f69ac1fc4cfd3ef89003c973cd614
Andreas Lind <andreaslindpetersen@gmail.com>
2018-03-14 23:06:44 +0100

    Add a test

--- test/index.js
+++ test/index.js
@@ -1,58 +1,13 @@
 /*global describe, it, console:true*/
-var expect = require('unexpected').clone().use(require('unexpected-mitm'));
-var hyperlink = require('../lib/');
-
-var originalProcessExit = process.exit;
-expect.addAssertion('to call process.exit [with]', function (expect, subject, value) {
-    var flags = this.flags;
-    var that = this;
-    return expect.promise(function (run) {
-        process.exit = run(function (exitCode) {
-            process.exit = originalProcessExit;
-            if (flags.with || typeof value !== 'undefined') {
-                that.errorMode = 'nested';
-                expect(exitCode, 'to equal', value);
-                that.errorMode = 'default';
-            }
-        });
-        subject();
-    });
-});
-
-var originalConsole = console;
-expect.addAssertion('with mock console', function (expect, subject, value) {
-process.stdout.write('mocking\n');
-    console = {};
-    var argumentsByMethodName = {};
-    ['info', 'log', 'warn', 'error'].forEach(function (methodName) {
-        argumentsByMethodName[methodName] = [];
-        console[methodName] = function () {
-process.stdout.write(methodName + JSON.stringify([].slice.call(arguments)) + '\n');
-            argumentsByMethodName[methodName].push(arguments);
-        };
-    });
-    return expect.promise(function () {
-        return this.shift(subject, 1);
-    }).then(function () {
-process.stdout.write('then\n');
-
-        console = originalConsole;
-        expect(argumentsByMethodName, 'to satisfy', value);
-    }).caught(function (err) {
-process.stdout.write('caught\n');
-        console = originalConsole;
-        expect.fail(err);
-process.stdout.write('reinstating\n');
-    });
-});
+const expect = require('unexpected').clone().use(require('unexpected-sinon'));
+const hyperlink = require('../lib/');
+const httpception = require('httpception');
+const TapRender = require('tap-render');
+const sinon = require('sinon');
 
 describe('hyperlink', function () {
-    it('should complain about insecure content warnings', function () {
-        return expect(hyperlink.bind(hyperlink, {
-            recursive: false,
-            root: 'https://example.com/',
-            inputUrls: [ 'https://example.com/' ]
-        }), 'with http mocked out', [
+    it('should complain about insecure content warnings', async function () {
+        httpception([
             {
                 request: 'GET https://example.com/',
                 response: {
@@ -64,7 +19,7 @@ describe('hyperlink', function () {
                 }
             },
             {
-                request: 'GET http://example.com/insecureScript.js',
+                request: 'HEAD http://example.com/insecureScript.js',
                 response: {
                     statusCode: 200,
                     headers: {
@@ -73,8 +28,19 @@ describe('hyperlink', function () {
                     body: 'alert("hello, insecure world");'
                 }
             }
-        ], 'with mock console', {
-            log: [ 'vqewvqwe']
-        }, 'to call process.exit with', 0);
+        ]);
+
+        const t = new TapRender();
+        sinon.spy(t, 'push');
+        await hyperlink({
+            recursive: false,
+            root: 'https://example.com/',
+            inputUrls: [ 'https://example.com/' ]
+        }, t);
+
+        expect(t.close(), 'to satisfy', {fail: 1});
+        expect(t.push, 'to have a call satisfying', () => {
+            t.push(null, { name: 'URI should be secure - http://example.com/insecureScript.js' });
+        });
     });
 });
